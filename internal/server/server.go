1a065ef6c7fe3d73e08bd6b023c26c9a5fcf066f
alex <alexyoung0119@gmail.com>
2025-07-08 21:31:13 -0500

    log success

--- internal/server/server.go
+++ internal/server/server.go
@@ -12,21 +12,21 @@ import (
 )
 
 func Run() {
-	log.Print("Loading environment variables...")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading environment: %v", err)
 	}
+	log.Print("Successfully loaded environment")
 	PORT := os.Getenv("PORT")
 
-	log.Print("Initializing database...")
 	db.Init()
-	log.Print("Initializing OAuth2 client...")
+	log.Print("Initialized database")
 	auth.Init()
+	log.Print("Initialized OAuth2 client")
 
-	log.Print("Initializing server...")
 	mux := http.NewServeMux()
 	handlers.Init(mux)
+	log.Print("Initialized handlers")
 
 	log.Println("Server starting on port :" + PORT)
 	log.Fatal(http.ListenAndServe(":"+PORT, mux))
