2c03182bc90d2e222090282b6497195fa277587d
Alex Young <alexyoung0119@gmail.com>
2025-08-29 22:22:07 -0500

    simplify

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -31,6 +31,18 @@ Google Calendar via through natural language.`,
 			auth.GetClient()
 		},
 	}
+
+	configCmd = &cobra.Command{
+		Use:   "config",
+		Short: "Configure agc",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
+
+	queryCmd = &cobra.Command{
+		Use:   "config",
+		Short: "Configure agc",
+		Run:   func(cmd *cobra.Command, args []string) {},
+	}
 )
 
 func init() {
@@ -41,6 +53,43 @@ func init() {
 
 	rootCmd.AddCommand(configCmd)
 	rootCmd.AddCommand(authCmd)
+}
+
+func initConfig() {
+	home, err := os.UserHomeDir()
+	cobra.CheckErr(err)
+	defaultPath = home + defaultPath
+
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Search in $HOME/.config/agc for agc.conf
+		viper.AddConfigPath(defaultPath)
+		viper.SetConfigType(configType)
+		viper.SetConfigName(configName)
+	}
+
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		// if configFile not set and default not found, create default
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok && cfgFile == "" {
+			// mkdir
+			if err := os.MkdirAll(defaultPath, os.ModePerm); err != nil {
+				cobra.CheckErr(err)
+			}
+
+			// create file for user
+			err = viper.SafeWriteConfig()
+			if err != nil {
+				cobra.CheckErr(err)
+			}
+			// read in new config
+			viper.ReadInConfig()
+		} else {
+			cobra.CheckErr(err)
+		}
+	}
 }
 
 func Execute() {
--- internal/cli/query.go
+++
@@ -1,8 +0,0 @@
-package cli
-
-import "github.com/spf13/cobra"
-
-var queryCmd = &cobra.Command{
-	Use:   "query",
-	Short: "Query AGC with natural language",
-}
