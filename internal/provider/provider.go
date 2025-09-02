e3f9f8b29d13cc43e3c23a7a8824fe0d0a3fb298
Alex Young <alexyoung0119@gmail.com>
2025-09-01 21:37:32 -0500

    try to update provider definitions

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,14 +2,25 @@ package provider
 
 import "context"
 
-type Client interface {
-	GenerateContent(ctx context.Context, model string, contents []*Content, config *GenerationConfig)
+type Provider interface {
+	NewClient() Client
 }
 
+type Client interface {
+	NewChat(ctx context.Context, model string, config *GenerationConfig, history []*Content) (Chat, error)
+}
+
+type Chat interface {
+	SendMessage(ctx context.Context, prompt string) (*ContentResponse, error)
+}
+
+// GenerationConfig is a struct that contains configuration options for the generation process.
+// e.g. Temperature, System Instructions, tools, etc.
 type GenerationConfig struct{}
 
 type Tool struct{}
 
+// ContentResponse is a struct that contains the response from the model.
 type ContentResponse struct{}
 
 type Content struct{}
