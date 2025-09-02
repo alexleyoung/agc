1f054c0c0d8d7cbc90d69369331c4cf70f06f454
Alex Young <alexyoung0119@gmail.com>
2025-09-01 20:58:31 -0500

    remove random code from main

--- cmd/agc/agc.go
+++ cmd/agc/agc.go
@@ -9,10 +9,5 @@ import (
 )
 
 func main() {
-	cals, _ := calendar.ListCalendars(context.Background())
-	var cal_strings string
-	for _, cal := range cals {
-		cal_strings += fmt.Sprintf("%s\n", cal.Summary)
-	}
 	cli.Execute()
 }
